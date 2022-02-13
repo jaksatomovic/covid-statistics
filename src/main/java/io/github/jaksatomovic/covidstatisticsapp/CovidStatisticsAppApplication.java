601f21853659332fbb41ef7237ba6f21d3dc7b3d
Jaska Tomovic <jaksa.tomovic@cyansecurity.com>
2022-02-13 18:10:31 +0100

    [INIT]

---
+++ src/main/java/io/github/jaksatomovic/covidstatisticsapp/CovidStatisticsAppApplication.java
@@ -0,0 +1,13 @@
+package io.github.jaksatomovic.covidstatisticsapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CovidStatisticsAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CovidStatisticsAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/jaksatomovic/covidstatisticsapp/CovidStatisticsAppApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.jaksatomovic.covidstatisticsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CovidStatisticsAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
